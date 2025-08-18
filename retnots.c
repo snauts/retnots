41201b058278e7ed46f8f338946720526412eda3
Artūrs Grebstelis <arturs@mikrotik.com>
2025-08-18 13:32:48 +0300

    add NES hw init

--- retnots.c
+++ retnots.c
@@ -8,6 +8,11 @@ void sdcc_deps(void) __naked {
     __asm__(".area ZP (PAG)");
     __asm__("REGTEMP:		.ds 8");
     __asm__("DPTR:		.ds 2");
+    __asm__("_ppu_ptr:		.ds 2");
+    __asm__("_ppu_count:	.ds 1");
+    __asm__("_ppu_buffer:	.ds 32");
+    __asm__("_counter:		.ds 1");
+    __asm__("_signal:		.ds 1");
 
     __asm__(".area OAM (PAG)");
     __asm__("_oam:		.ds 256");
@@ -46,10 +51,107 @@ void rst(void) __naked {
     __asm__("jsr _game_startup");
 }
 
+#define BIT(n)		(((byte) 1) << (n))
+
+#define MEM_RD(a)	(* (volatile byte *) (a))
+#define MEM_WR(a, x)	(* (volatile byte *) (a) = (x))
+
+#define PPUCTRL(x)	MEM_WR(0x2000, x)
+#define PPUMASK(x)	MEM_WR(0x2001, x)
+#define PPUSTATUS()	MEM_RD(0x2002)
+#define OAMADDR(x)	MEM_WR(0x2003, x)
+#define OAMDATA(x)	MEM_WR(0x2004, x)
+#define PPUSCROLL(x)	MEM_WR(0x2005, x)
+#define PPUADDR(x)	MEM_WR(0x2006, x)
+#define PPUDATA(x)	MEM_WR(0x2007, x)
+
+#define DMCFREQ(x)	MEM_WR(0x4010, x)
+#define OAMDMA(x)	MEM_WR(0x4014, x)
+#define SND_CHN(x)	MEM_WR(0x4015, x)
+#define JOY2_WR(x)	MEM_WR(0x4017, x)
+
+extern byte oam[256];
+
+extern volatile word ppu_ptr;
+extern volatile byte ppu_count;
+extern volatile byte ppu_buffer[32];
+
+extern volatile byte counter;
+extern volatile byte signal;
+
+static void wait_vblank(void) {
+    while ((PPUSTATUS() & 0x80) == 0) { }
+}
+
+static void wait_signal(void) {
+    signal = 1;
+    while (signal) { }
+}
+
+static void memset(byte *buf, byte val, word count) {
+    while (count-- > 0) *buf++ = val;
+}
+
+static void memcpy(byte *dst, byte *src, word count) {
+    while (count-- > 0) *dst++ = *src++;
+}
+
+static void clear_palette(void) {
+    PPUADDR(0x3f);
+    PPUADDR(0x00);
+    for (byte i = 0; i < 32; i++) {
+	PPUDATA(0x0f);
+    }
+}
+
+static void init_memory(void) {
+    counter = 0;
+    ppu_count = 0;
+
+    memset(oam, 255, 0x100);
+}
+
+static void ppu_ctrl(void) {
+    PPUSCROLL(0x00);
+    PPUSCROLL(0x00);
+    PPUCTRL(BIT(7) | BIT(3));
+}
+
 void irq_handler(void) {
+    byte i;
+    PPUADDR(ppu_ptr >> 8);
+    PPUADDR(ppu_ptr & 0xff);
+    for (i = 0; i < ppu_count; i++) {
+	PPUDATA(ppu_buffer[i]);
+    }
+    ppu_count = 0;
+    counter++;
+
+    PPUMASK(0x00);
+    OAMADDR(0x00);
+    OAMDMA(0x02); /* oam addr high byte */
+    ppu_ctrl();
+    PPUMASK(0x1e);
+    signal = 0;
+}
+
+static void hw_init(void) {
+    JOY2_WR(0x40);
+    PPUCTRL(0x00);
+    PPUMASK(0x00);
+    DMCFREQ(0x00);
+    SND_CHN(0x0f);
+
+    wait_vblank();
+    init_memory();
+    wait_vblank();
+    clear_palette();
+    wait_vblank();
+    ppu_ctrl();
 }
 
 void game_startup(void) {
+    hw_init();
 }
 
 /* must be very last */
