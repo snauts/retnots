73a33dad7f83f873533d2e826212d94d66a695c5
Artūrs Grebstelis <arturs@mikrotik.com>
2025-08-26 16:48:30 +0300

    change default hi-score table

--- retnots.c
+++ retnots.c
@@ -142,7 +142,7 @@ extern byte bump;
 extern int8 safe;
 extern byte pos;
 
-static const char default_table[] = "HORACE 0300TOMBA  0200JENOTS 0100";
+static const char default_table[] = "HORACE 0300JENOTS 0200ARCHA  0100";
 
 static byte table[sizeof(default_table)];
 
