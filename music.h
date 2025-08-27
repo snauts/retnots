05518c87169a90c0b6e6f5d3a02baa12fff12f0d
Arturs Grebstelis <arturs.grebstelis@gmail.com>
2025-08-27 19:48:25 +0300

    simplify description of note sheet

--- music.h
+++ music.h
@@ -1,17 +1,17 @@
-#define NOTE(p, l)	(0x40 | (5 * p)), (l)
+#define NOTE(p)		(0x40 | (5 * p))
 
-#define C(l)		NOTE(0x0, l)
-#define c(l)		NOTE(0x1, l)
-#define D(l)		NOTE(0x2, l)
-#define d(l)		NOTE(0x3, l)
-#define E(l)		NOTE(0x4, l)
-#define F(l)		NOTE(0x5, l)
-#define f(l)		NOTE(0x6, l)
-#define G(l)		NOTE(0x7, l)
-#define g(l)		NOTE(0x8, l)
-#define A(l)		NOTE(0x9, l)
-#define a(l)		NOTE(0xa, l)
-#define B(l)		NOTE(0xb, l)
+#define Cn		NOTE(0x0)
+#define Cs		NOTE(0x1)
+#define Dn		NOTE(0x2)
+#define Ds		NOTE(0x3)
+#define En		NOTE(0x4)
+#define Fn		NOTE(0x5)
+#define Fs		NOTE(0x6)
+#define Gn		NOTE(0x7)
+#define Gs		NOTE(0x8)
+#define An		NOTE(0x9)
+#define As		NOTE(0xa)
+#define Bn		NOTE(0xb)
 
 static const byte chord_D[] = { 20, 60, 90 };
 static const byte chord_A[] = { 90, 130, 160 };
@@ -20,25 +20,25 @@ static const byte chord_A[] = { 90, 130, 160 };
 #define cA		0x81
 
 static const byte melody[] = {
-    cD, A(2), f(2),
-        A(2), f(2),
-    cA, G(1), E(1), G(1), E(1),
-    cD, f(2), f(2),
+    cD, An, 2, Fs, 2,
+        An, 2, Fs, 2,
+    cA, Gn, 1, En, 1, Gn, 1, En, 1,
+    cD, Fs, 2, Fs, 2,
 
-    cD, A(2), f(2),
-        A(2), f(2),
-    cA, G(1), E(1), G(1), E(1),
-    cD, f(2), f(2),
+    cD, An, 2, Fs, 2,
+        An, 2, Fs, 2,
+    cA, Gn, 1, En, 1, Gn, 1, En, 1,
+    cD, Fs, 2, Fs, 2,
 
-    cA, E(1), E(1), E(1), E(1),
-    cD, f(1), f(1), f(1), f(1),
-    cA, G(1), f(1), G(1), E(1),
-    cD, D(1), D(1), D(2),
+    cA, En, 1, En, 1, En, 1, En, 1,
+    cD, Fs, 1, Fs, 1, Fs, 1, Fs, 1,
+    cA, Gn, 1, Fs, 1, Gn, 1, En, 1,
+    cD, Dn, 1, Dn, 1, Dn, 2,
 
-    cA, E(1), E(1), E(1), E(1),
-    cD, f(1), f(1), f(1), f(1),
-    cA, G(1), f(1), G(1), E(1),
-    cD, D(1), D(1), D(2),
+    cA, En, 1, En, 1, En, 1, En, 1,
+    cD, Fs, 1, Fs, 1, Fs, 1, Fs, 1,
+    cA, Gn, 1, Fs, 1, Gn, 1, En, 1,
+    cD, Dn, 1, Dn, 1, Dn, 2,
 
     0,
 };
