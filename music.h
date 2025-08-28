aadb05a2f58a45b828d3946c518d8f2aa90ad5b3
Artūrs Grebstelis <arturs@mikrotik.com>
2025-08-28 13:14:19 +0300

    add arpeggio chords to music

--- music.h
+++ music.h
@@ -2,7 +2,10 @@
 #define N(p)		(0x40 | (4 * p))
 
 #define cD		(0x80 | (4 * 2))
+#define cFs		(0x80 | (4 * 6))
 #define cA		(0x80 | (4 * 9))
+#define cCs		(0x80 | (4 * 1 + 1))
+#define cE		(0x80 | (4 * 4 + 1))
 
 #define Cn		N(0x0)
 #define Cs		N(0x1)
@@ -18,25 +21,24 @@
 #define Bn		N(0xb)
 
 static const byte melody[] = {
-    cD, An, R(0), 24,               Fs, R(9), 24,
-        An, R(0), 24,               Fs, R(9), 24,
-    cA, Gn, R(0), 12, En,       12, Gn, R(9), 12, En,       12,
-    cD, Fs, R(0), 24,               Fs, R(9), 24,
+    cD, An, R(0), 3, cFs, 3, cA, 6,     R(9), 12, Fs, R(0), 12,     R(9), 12,
+        An, R(0),               12,     R(9), 12, Fs, R(0), 12,     R(9), 12,
+    cA, Gn, R(0), 3, cCs, 3, cE, 6, En, R(9), 12, En, R(0), 12, Gn, R(9), 12,
+    cD, Fs, R(0), 3, cFs, 3, cA, 6,     R(9), 12, Fs, R(0), 12,     R(9), 12,
 
-    cD, An, R(0), 24,               Fs, R(9), 24,
-        An, R(0), 24,               Fs, R(9), 24,
-    cA, Gn, R(0), 12, En,       12, Gn, R(9), 12, En,       12,
-    cD, Fs, R(0), 24,               Fs, R(9), 24,
+    cD, An, R(0), 3, cFs, 3, cA, 6,     R(9), 12, Fs, R(0), 12,     R(9), 12,
+        An, R(0),               12,     R(9), 12, Fs, R(0), 12,     R(9), 12,
+    cA, Gn, R(0), 3, cCs, 3, cE, 6, En, R(9), 12, En, R(0), 12, Gn, R(9), 12,
+    cD, Fs, R(0), 3, cFs, 3, cA, 6,     R(9), 12, Fs, R(0), 12,     R(9), 12,
 
-    cA, En, R(0), 12, En, R(9), 12, En, R(0), 12, En, R(9), 12,
-    cD, Fs, R(0), 12, Fs, R(9), 12, Fs, R(0), 12, Fs, R(9), 12,
-    cA, Gn, R(0), 12, Fs, R(9), 12, Gn, R(0), 12, En, R(9), 12,
-    cD, Dn, R(0), 12, Dn, R(9), 12, Dn, R(9), 24,
+    cA, En, R(0), 3, cCs, 3, cE, 6, En, R(9), 12, En, R(0), 12, En, R(9), 12,
+    cD, Fs, R(0), 3, cFs, 3, cA, 6, Fs, R(9), 12, Fs, R(0), 12, Fs, R(9), 12,
+    cA, Gn, R(0), 3, cCs, 3, cE, 6, Fs, R(9), 12, Gn, R(0), 12, En, R(9), 12,
+    cD, Dn, R(0), 3, cFs, 3, cA, 6, Dn, R(9), 12, Dn, R(0), 12,     R(9), 12,
 
-    cA, En, R(0), 12, En, R(9), 12, En, R(0), 12, En, R(9), 12,
-    cD, Fs, R(0), 12, Fs, R(9), 12, Fs, R(0), 12, Fs, R(9), 12,
-    cA, Gn, R(0), 12, Fs, R(9), 12, Gn, R(0), 12, En, R(9), 12,
-    cD, Dn, R(0), 12, Dn, R(9), 12, Dn, R(9), 24,
-
+    cA, En, R(0), 3, cCs, 3, cE, 6, En, R(9), 12, En, R(0), 12, En, R(9), 12,
+    cD, Fs, R(0), 3, cFs, 3, cA, 6, Fs, R(9), 12, Fs, R(0), 12, Fs, R(9), 12,
+    cA, Gn, R(0), 3, cCs, 3, cE, 6, Fs, R(9), 12, Gn, R(0), 12, En, R(9), 12,
+    cD, Dn, R(0), 3, cFs, 3, cA, 6, Dn, R(9), 12, Dn, R(0), 12,     R(9), 12,
     0,
 };
