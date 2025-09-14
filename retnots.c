093f30ffde199f5e398a8f0cb7ab19fee632ec71
Arturs Grebstelis <arturs.grebstelis@gmail.com>
2025-09-14 12:36:04 +0300

    faster jumping star animation framerate

--- retnots.c
+++ retnots.c
@@ -707,7 +707,7 @@ static void check_controls(void) {
 	i = counter & 0x10 ? 0x0c : 0x3c;
     }
     else if (bump == JUMP) {
-	i = counter & 0x10 ? 0x66 : 0x96;
+	i = counter & 0x08 ? 0x66 : 0x96;
     }
     else if (bump > SPECIAL) {
 	i = 0x60;
